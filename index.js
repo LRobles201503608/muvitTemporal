17d67327109e3337d0a84df8e5597910f9fcc63c
Juan Luis Robles Molina <61329169+LRobles201503608@users.noreply.github.com>
2022-04-28 20:52:59 -0600

    Add files via upload

--- index.js
+++ index.js
@@ -1,9 +1,15 @@
 function openNav() {
-  document.getElementById("mySidebar").style.width = "250px";
-  document.getElementById("main").style.marginLeft = "250px";
+  document.getElementById("mySidebar").style.width = "100%";
+  document.getElementById("main").style.marginLeft = "100%";
+  document.getElementById("opennav").style.display  = "none";
 }
 
-function closeNav() {
+function closeNav(position) {
   document.getElementById("mySidebar").style.width = "0";
   document.getElementById("main").style.marginLeft= "0";
+  location.href=position
+}
+
+function redirect_instagram(){
+  window.open("https://instagram.com/muvit_travel?igshid=YmMyMTA2M2Y=","_blank")
 }
